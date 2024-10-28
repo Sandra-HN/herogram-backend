d728d2b84999d733b9502900c4c8ccae5ca7c268
Sandra Hnaidy <sandra.altheeb.3@gmail.com>
2024-10-28 14:19:23 +0400

    allowedOrigins

--- index.js
+++ index.js
@@ -12,7 +12,25 @@ const PORT = process.env.PORT || 5000; // Use the port from the .env file or def
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://herogram-frontend.vercel.app",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use("/uploads", express.static("uploads"));
 // Routes
